29c64b18d06dc918f83a4e014a6faa30b5be0732
ginpei <ginpei@gmail.com>
2018-02-11 11:45:47 -0800

    extract path list

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,14 @@
+const paths = [
+  '/',
+  '/index.html',
+  '/js.js',
+  '/css.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css',
+]
+
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open('static').then((cache) => {
-    cache.addAll([
-      '/',
-      '/index.html',
-      '/js.js',
-      '/css.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css',
-    ])
+    cache.addAll(paths)
   }))
 })
 
