8d94e539d2379a80277067ee830299dfa14f2ec9
ginpei <ginpei@gmail.com>
2018-02-24 23:49:54 -0800

    update

--- js.js
+++ js.js
@@ -6,11 +6,13 @@ class TaskController {
   constructor (options = {}) {
     this.elText = options.elApp || document.querySelector('#text')
     this.elRevert = options.elRevert || document.querySelector('#revert')
+    this.elMessage = options.elMessage || document.querySelector('#message')
     this.initialData = {}
   }
 
   start () {
     this.load()
+    this.showMessage('READY')
 
     this.elText.addEventListener('input', (event) => {
       this.save()
@@ -29,9 +31,15 @@ class TaskController {
   }
 
   saveData ({ text }) {
+    this.showMessage('SAVING')
+
     const data = { text }
     const json = JSON.stringify(data)
     localStorage.setItem('mypwa-data', json)
+
+    setTimeout(() => {
+      this.showMessage('SAVED')
+    }, 300)
   }
 
   load () {
@@ -50,6 +58,27 @@ class TaskController {
     console.log('revert')
     this.saveData(this.initialData)
     this.load()
+  }
+
+  showMessage (id) {
+    let message = null
+    if (id === 'READY') {
+      message = 'Ready.'
+    }
+    else if (id === 'SAVING') {
+      message = 'Saving...'
+    }
+    else if (id === 'SAVED') {
+      message = `Saved at ${new Date().toString()}`
+    }
+    else {
+      console.warn('id', id)
+      throw new Error('Unknown message id.')
+    }
+
+    if (message) {
+      this.elMessage.textContent = message
+    }
   }
 }
 
