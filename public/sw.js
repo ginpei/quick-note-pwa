0834b0fa3218a85bb586f9256ddbc4080dbb359f
ginpei <ginpei@gmail.com>
2018-02-24 23:48:15 -0800

    better caching strategy

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,58 @@
 const paths = [
   '/',
-  '/index.html',
   '/js.js',
   '/css.css',
   'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css',
 ]
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open('static').then((cache) => {
-    cache.addAll(paths)
-  }))
-})
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(caches.open('static').then((cache) => {
+//     cache.addAll(paths)
+//   }))
+// })
 
 // self.addEventListener('activate', (event) => {
 //   console.log('SW activated!')
 // })
 
+function isCacheTarget (sUrl) {
+  return paths.includes(sUrl) || paths.includes(new URL(sUrl).pathname)
+}
+
 self.addEventListener('fetch', (event) => {
+  // - offline -> returns cache even if not cached
+  // - online && not cached -> fetch then cache and return it
+  // - online && cached -> return it while fetching and updating cache
+
   const req = event.request
-  event.respondWith(caches.match(req).then((res) => {
-    let p
+  if (!isCacheTarget(req.url)) {
+    return
+  }
+
+  event.respondWith((async () => {
+    let res = await caches.match(req.url)
+
+    let pFetched = null
     if (navigator.onLine) {
-      p = fetch(req)
-      p.then(async () => {
-        const cache = await caches.open('static')
-        cache.add(req.url)
-      })
+      // for browser-sync
+      // (this doesn't affect production env, however)
+      const init = {}
+      if (new URL(req.url).pathname === '/') {
+        init.headers = { Accept: 'text/html' }
+      }
+
+      pFetched = fetch(req.url, init)
+        .then(async (res) => {
+          const cache = await caches.open('static')
+          cache.put(req.url, res)
+        })
     }
 
-    return res || p
-  }))
+    // wait if online and not cached
+    if (pFetched && !res) {
+      res = await pFetched
+    }
+
+    return res
+  })())
 })
